aa52f52301c4b3d1be92ccce25ab30747b4ee274
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-03-15 07:48:30 -0300

    chore: initializing tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import styled from 'styled-components'
-import { GlobalStyle } from './styles/global'
 import { Header } from './components/headers/header'
 
 export function App(){
     return (
       <>
         <Header />
-        <GlobalStyle />
       </>
     )
 }
--- src/components/headers/header.jsx
+++ src/components/headers/header.jsx
@@ -1,14 +1,9 @@
 import SetaSair from '../../assets/SetaSairEdit.svg'
-import { Container, Exit } from '../styles/styles'
 
 export function Header() {
     return (
-        <Container>
-            <Exit>
-                <a href="">
-                    <img src={SetaSair} alt="Sair da página"/>
-                </a>
-            </Exit>
-        </Container>
+        <a href="">
+            <img src={SetaSair} alt="Sair da página"/>
+        </a>
     )
 }
