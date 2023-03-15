0fdd828a8a14c60f90d8c0c5e63243d8d11b1cdb
GyovanneMS <Gyovannecontato@gmail.com>
2023-03-15 09:38:03 -0300

    15/03/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components'
-import { Header } from './components/headers/header'
+
+import { Header } from './components/headers/headerEdits'
 
 export function App(){
     return (
