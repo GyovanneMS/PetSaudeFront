f5428010bbf3fb63180ac7e76c41706f0168f171
Yasmin Santos <yasminnas989@gmail.com>
2023-03-16 11:28:30 -0300

    update

---
+++ src/pages/home/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export const LandingPage = () => {
+  return (
+    <>
+       <h1>Hello</h1>
+      <span>teste</span>
+    </>
+  );
+};
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {CadastroVet} from "./pages/cadastroVet/cadastroVet";
 import { Casa } from "./pages/home/Home";
-import {LaddingPage} from "./pages/home/LadingPage"
+import { LandingPage} from "./pages/home/LandingPage"
 import { Main } from "./pages/login/mainLogin";
 import { NewAdress } from "./pages/login/mainAddAdress";   
 
@@ -14,7 +14,7 @@ export const Rotas = () => {
             <Route path="/" element={<Casa />}></Route>
             <Route path="/pages/Endereco" element={<NewAdress />} />
             <Route path="/pages/CadastroVet" element={<CadastroVet />} />
-            <Route path="/home" element={<LaddingPage />}></Route>
+            <Route path="/home" element={<LandingPage />}></Route>
         </Routes>
     </Router>
   );
