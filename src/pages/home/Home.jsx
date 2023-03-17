15332b6f799bdb9007762c51ff8376938cb56fba
srmt03 <samuelrodrigues.mt05@gmail.com>
2023-03-17 11:21:47 -0300

    login page in progress

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,16 +10,10 @@ export const Casa = () => {
         <p>home</p>
         <Link to="/pages/CadastroVet">Cadastro do veterinario</Link>
         <Link to="/pages/login">Login do usuario</Link>
-<<<<<<< HEAD
-        <Link to="/pages/address">Login do usuario</Link>
-        <Link to="/pages/LadingPage">Lading page</Link>
-
-=======
         <Link to="/pages/Endereco">Endereço do usuario</Link>
         <Link to="/login/mainLogin">Main Login</Link>
         <Link to="/home/HeaderWeb">Header das telas Web</Link>
         <Link to="/home/LandingPage">Landing Page</Link>
->>>>>>> d5fe843ca6161040ec0eabb9f3ae9846e3de2a36
     </>
   );
 };
