512dd34bc26110efc8f6f8c7c19e074f29ac7718
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-03-18 23:24:50 -0300

    route file formatted

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {CadastroVet} from "./pages/cadastroVet/cadastroVet";
 import { HomePage } from "./pages/home/Home";
-import { Register } from "./pages/register/register";
+import { Register } from "./pages/register/Register";
 import { HeaderWeb } from "./pages/home/resource/HeaderWeb";
 import { LandingPage} from "./pages/home/LandingPage";
-import { Login } from "./pages/login/mainLogin";
+import { Login } from "./pages/login/Login";
 import {NewAdress} from "./pages/register/resource/mainAddAddress";
 
 
@@ -14,13 +14,13 @@ export const Rotas = () => {
   return (
     <Router>
         <Routes>
-            <Route path="/pages/login" element={<Login />} />
             <Route path="/" element={<HomePage />}></Route>
-            <Route path="/pages/Endereco" element={<NewAdress />} />
-            <Route path="/pages/CadastroVet" element={<CadastroVet />} />
-            <Route path="/home/LandingPage" element={<LandingPage />}/>
+            <Route path="/home" element={<LandingPage />}/>
             <Route path="/home/HeaderWeb" element={<HeaderWeb />}/>
+            <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />}/>
+            <Route path="/register/address" element={<NewAdress />} />
+            <Route path="/register/veterinary" element={<CadastroVet />} />
         </Routes>
     </Router>
     
