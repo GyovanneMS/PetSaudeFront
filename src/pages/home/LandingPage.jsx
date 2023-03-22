1345e0fc1a5befdb20885c26327cbac2dfe342ab
Yasmin Santos <yasminnas989@gmail.com>
2023-03-22 11:25:26 -0300

    responsive intro

--- src/pages/home/LandingPage.jsx
+++ src/pages/home/LandingPage.jsx
@@ -5,40 +5,42 @@ import Footprint from "../../assets/svg/petPaws.svg";
 import Dog from "../../assets/svg/dogAndCat.svg";
 import Doctor from "../../assets/svg/medico 1.svg";
 import Local from "../../assets/svg/localizacao.svg";
+import "./css/LandingPage.css";
 
 
 export const LandingPage = () => {
   return (
-    <div>
-      <HeaderWeb/>
-    <div className=" home flex flex-row justify-center space-x-20 p-5 font-normal texto-2xl " >
-      <div className=" search flex flex-row gap-5 block bg-white w-1/4 border rounded-lg border-black border-2 hover:border-green-200  p-7 w-full">
+    <div >
+       <HeaderWeb />
+    
+    <div className=" home flex flex-row justify-center xl: space-x-20 p-10 font-normal texto-2xl " >
+      <div className=" search flex flex-row gap-5  bg-white w-1/4 border rounded-lg border-black hover:border-green-200  p-5  ">
         <img src={Doctor} />
-        <input placeholder="Pesquisar especialistas"/>
+        <input className="w-full h-full text-2xl" placeholder="Pesquisar especialistas"/>
       </div>
 
-      <div className=" search flex flex-row gap-5 block bg-white w-1/4 border rounded-lg border-black border-2 hover:border-green-200  p-5">
+      <div className=" flex flex-row bg-white w-1/4 border rounded-lg border-black hover:border-green-200  p-5">
         <img src={Local}/>
-        <input placeholder="Pesquisar especialistas"/>
+        <input className="w-full h-full text-2xl" placeholder="Pesquisar veterinários próximos"/>
       </div>
     
     </div>
 
-      <div className="flex flex-row justify-between content center w-auto h-4/5 mt-10 ">
+      <div className="flex flex-row justify-between content center w-auto h-4/5 mt-10  ">
 
-        <div className="basis-1/2 flex flex-col justify-center items-center ">
-          <h1 className="basis-1/2 flex justify-center items-center text-7xl w-3/5 h-30 font-bold ">Agende suas consultas e forneça o melhor para o seu Pet!</h1>
-          <Link className="basis-1/4 w-96 text-3xl flex text-center justify-center items-center border border-2 rounded-3xl border-[#9ED1B7]" to="/">Procure um veterinário próximo!</Link>
+        <div className=" flex flex-col justify-center items-center">
+          <h1 className="basis-1/2 flex justify-center items-center font-bold text-6xl  xl:text-7xl w-3/5 ">Agende suas consultas e forneça o melhor para o seu Pet!</h1>
+          <Link className="flex text-center justify-center items-center border-2 rounded-3xl border-[#9ED1B7] h-30 w-50 basis-1-6 mt-10 xl:h-20 w-96 basis-1/6 text-3xl " to="/">Procure um veterinário próximo!</Link>
         </div>
 
 
-        <div className="flex justify-end content-center basis-1/2">
-          <img className="w-4/5 pt-20 " src={Dog}/>
+        <div className="flex justify-end content-center basis-1/2 lg:bg-none">
+          <img className="mt-10 w-4/5 " src={Dog}/>
         </div>
     </div>
 
-    <div className="pl-">
-      <img className=" w-2/4 " src={Footprint}/>
+    <div>
+      <img className=" w-3/2 mt-20 xl:w-2/4" src={Footprint}/>
     </div>
     </div>
   );
--- src/pages/home/resource/HeaderWeb.jsx
+++ src/pages/home/resource/HeaderWeb.jsx
@@ -5,25 +5,35 @@ import Photo from '../../../assets/svg/userAnonimo.svg';
 
 export const HeaderWeb = () => {
   return (
-    <div className="flex flex-direction space-x-20 p-10 font-bold
-    text-4xl  justify-between pr-20 pl-20 pr-5.5">
-       <h1>PetSaúde</h1>
-       <nav class="navigation">
-            <ul className="flex flex-direction  space-x-10 font-normal text-2xl h-25 ">
-                <li className=" hover:bg-green-100 rounded-lg ...">
-                  <a href="./LandingPage.jsx">Home</a>
-                </li>
-                <li className=" hover:bg-green-100 rounded-lg ...">
-                  <a href="#">Profissionas</a>
-                  </li>
-                <li className=" hover:bg-green-100 rounded-lg ...">
-                  <a href="#">Blog</a>
-                </li>
-                <li className=" hover:bg-green-100 rounded-lg ...">
-                  <a href="#">Sobre nós</a>
-                </li>
-                <i class='bx bx-search'></i>
-            </ul>
+    <div className="flex  font-normal
+     justify-between bg-white shadow md: md:items-center 
+    xl:space-x-20 p-10 text-4xl md:space-x-20  ">
+       <h1 className="sm:flex justify-start font-bold">PetSaúde</h1>
+       <nav>
+      <div class="flex justify-between items-center ">
+      <span class="text-3xl cursor-pointer mx-2 md:hidden block">
+        <ion-icon name="menu" onclick="Menu(this)"></ion-icon>
+      </span>
+      </div>
+            <ul class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
+              <li class="mx-4 my-6 md:my-0">
+                <a href="#" class="text-2xl hover:text-green-300 duration-500">Home</a>
+              </li>
+              <li class="mx-4 my-6 md:my-0">
+                <a href="#" class="text-2xl hover:text-green-300 duration-500">Profissionais</a>
+              </li>
+              <li class="mx-4 my-6 md:my-0">
+                <a href="#" class="text-2xl hover:text-green-300 duration-500">Blog</a>
+              </li>
+              <li class="mx-4 my-6 md:my-0">
+                <a href="#" class="text-2xl hover:text-green-300 duration-500">Sobre nós</a>
+              </li>
+          </ul>
+            <div class="menu">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
         </nav>
        
        <div className="flex flex-direction space-x-5 ">
