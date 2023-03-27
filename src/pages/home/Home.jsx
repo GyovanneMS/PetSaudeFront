66f036c3948c8646b455c6d55c4598f3ac830830
GyovanneMS <Gyovannecontato@gmail.com>
2023-03-27 10:32:26 -0300

    27/03/2023

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,7 +15,8 @@ export const HomePage = () => {
         <Link to="/register">Main Login</Link>/
         <Link to="/home/HeaderWeb">Header das telas Web</Link>/
         <Link to="/home">Landing Page</Link>/
-        <Link to="/profile/veterinary">profile veterinary</Link>
+        <Link to="/profile/veterinary">profile veterinary</Link>/
+        <Link to="/home/aboutUs">About Us</Link>
     </>
   );
   
