56241e7854560e93fd95689d912ba9689dac79fa
LUCAS CAMILO RAMALHO <98404472+LusCamilo@users.noreply.github.com>
2023-03-30 11:30:27 -0300

    update infos

--- src/pages/profile/resource/academicInfo.jsx
+++ src/pages/profile/resource/academicInfo.jsx
@@ -6,38 +6,38 @@ import carreira from './img/carreira.png'
 
 export const AcademicInfos = () => {
     return (
-        <div className='m-4'>
-            <h2 className="text-xl mb-3">
+        <div className='px-44 m-4'>
+            <h2 className="text-3xl mb-3">
                 Informações acadêmicas
             </h2>
-            <div className='w-8 h-8 border rounded-lg border-zinc-700'>
-                <div>
-                    <span>
-                        <h3></h3>
+            <div className='flex flex-col gap w-1/2 h-full border rounded-lg border-zinc-700'>
+                <div className='flex items-center border rounded-lg border-zinc-700'>
+                    <span className='flex flex-col'>
+                        <h3>Formação</h3>
                         <p></p>
                     </span>
-                    <img src={formacao} alt="" />
+                    <img className='p-4 border rounded-lg border-zinc-700' src={formacao} />
                 </div>
-                <div>
-                    <span>
-                        <h3></h3>
+                <div className='flex items-center border rounded-lg border-zinc-700'>
+                    <span className='flex flex-col'>
+                        <h3>Data de Formação</h3>
                         <p></p>
                     </span>
-                    <img src={formacaoDate} alt="" />
+                    <img className='p-4 border rounded-lg border-zinc-700' src={formacaoDate}  />
                 </div>
-                <div>
-                    <span>
-                        <h3></h3>
+                <div className='flex items-center border rounded-lg border-zinc-700'>
+                    <span className='flex flex-col'>
+                        <h3>Instituição</h3>
                         <p></p>
                     </span>
-                    <img src={instituicao} alt="" />
+                    <img className='p-4 border rounded-lg border-zinc-700' src={instituicao}  />
                 </div>
-                <div>
-                    <span>
-                        <h3></h3>
+                <div className='flex items-center border rounded-lg border-zinc-700'>
+                    <span className='flex flex-col'>
+                        <h3>Inicio de Carreira</h3>
                         <p></p>
                     </span>
-                    <img src={carreira} alt="" />
+                    <img className='p-4 border rounded-lg border-zinc-700' src={carreira}  />
                 </div>
             </div>
         </div>
