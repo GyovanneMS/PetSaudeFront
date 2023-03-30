ce8c9905c9f83df1dd743a59ee88acdc6b05ed01
GyovanneMS <Gyovannecontato@gmail.com>
2023-03-30 09:03:41 -0300

    30/03/2023

--- src/pages/profile/upgradeUser.jsx
+++ src/pages/profile/upgradeUser.jsx
@@ -3,7 +3,7 @@ import { Address } from './resource/editUser/address';
 import { CardPets } from './resource/editUser/cardPets';
 import { Pessoais } from './resource/editUser/infosPerson';
 import { Prossionais } from './resource/editUser/infosProfissional';
-
+import './css/UpgradeUser.css'
 
 export const UpgradeUser = () => {
 
@@ -13,10 +13,12 @@ export const UpgradeUser = () => {
             <div>
 
             </div>
-            <Address/>
-            <Pessoais/>
-            <CardPets/>
-            <Prossionais/>
+            <main>
+                <Pessoais/>
+                <Address cep="06045-420" bairro="Novo Osasco" rua="Via Transeversal Sul" cidade="Osasco" estado="São Paulo" complemento="46B"/>
+                <CardPets id=""/>
+                <Prossionais area="Psiquiatria de Pets" instituicao="USP" dataFormacao="20/09/2006" formacao="Zootecnia" crmv="1111" dataInicioAtuacao="20/08/2007"/>
+            </main>
         </>
     );
 }
