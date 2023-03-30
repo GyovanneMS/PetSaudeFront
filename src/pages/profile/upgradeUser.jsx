52c45a30e5e5cee25b3d8891020998cba958d22c
Yasmin Santos <yasminnas989@gmail.com>
2023-03-30 11:29:24 -0300

    29/03/2023

--- src/pages/profile/upgradeUser.jsx
+++ src/pages/profile/upgradeUser.jsx
@@ -3,7 +3,9 @@ import { Address } from './resource/editUser/address';
 import { CardPets } from './resource/editUser/cardPets';
 import { Pessoais } from './resource/editUser/infosPerson';
 import { Prossionais } from './resource/editUser/infosProfissional';
+import { Config } from './resource/editUser/headerConfig';
 import './css/UpgradeUser.css'
+
 
 export const UpgradeUser = () => {
 
@@ -18,6 +20,7 @@ export const UpgradeUser = () => {
                 <Pessoais name="Hayley" lastName="Williams" cpf="000.000.000-00" rg="000.000.000-00" celular="(88) 88888-8888 " telefone="(88) 88888-8888 "/>
                 <CardPets/>
                 <Prossionais area="Psiquiatria de Pets" instituicao="USP" dataFormacao="19/03/2005" formacao="Zootecnia" crmv="1234" dataInicioAtuacao="13/02/2006"/>
+                <Config hayley="Hayley Williams"/>
             </main>
         </>
     );
