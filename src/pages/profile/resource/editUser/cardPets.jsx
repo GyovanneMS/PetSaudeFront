59c5a941e210dadd5fb76b4bc2e210df1652adce
Yasmin Santos <yasminnas989@gmail.com>
2023-03-31 09:27:34 -0300

    31/03/2023

--- src/pages/profile/resource/editUser/cardPets.jsx
+++ src/pages/profile/resource/editUser/cardPets.jsx
@@ -7,8 +7,11 @@ export const CardPets = (props) => {
         <div className='max-w-xs h-64 border-4 border-[#CAC4D0] rounded-3xl'>
             <div className='h-1/4 flex flex-row justify-center content-center items-center px-5'>
                 <img src={props.personImage} alt="Imagem ou icone do perfil" className='w-10 h-10 rounded-full' />
-                <p className='font-sans font-medium text-base w-full px-5'> {props.animalName} </p>
+                <label>
+                <input type="text" name="firstName"value={props.animalName} className='bg-transparent border-none font-sans font-medium text-base w-full px-5 '/>
                 <img src={lapis} alt="Icone editar" className='w-5 h-5'/>
+                </label>
+                
             </div>
             <div className='w-full h-3/4 bg-[#CAC4D0] rounded-b-2xl'>
                 <img src={props.animalImage} alt="Imagem do pet" className="w-full h-full rounded-b-3xl object-cover"/>
--- src/pages/profile/resource/editUser/infosPerson.jsx
+++ src/pages/profile/resource/editUser/infosPerson.jsx
@@ -11,30 +11,34 @@ export const Pessoais = (props) => {
                 <div className='flex flex-row justify-between  pr-26'>
                     
                     <div className='flex flex-col gap-5 mt-10'>
-                        <p className=' text-xl text-[#A9A9A9]'> Primeiro nome
-                            <div className='text-3xl text-[#000]'>{props.name}</div>
-                        </p>
-                        <p className=' text-xl text-[#A9A9A9]'> CPF
-                            <div className='text-3xl text-[#000]'>{props.cpf}</div>
-                        </p>
-                        <p className=' text-xl text-[#A9A9A9]'> Celular
-                            <div className='text-3xl text-[#000]'> {props.celular}</div>
-                        </p>
+                        <label className='flex flex-col text-xl text-[#A9A9A9]'>
+                            Primeiro nome
+                            <input type="text" name="firstName" value={props.name} className='bg-transparent border-none text-3xl text-[#000]' />
+                        </label>
+                        <label className='flex flex-col text-xl text-[#A9A9A9]'>
+                           CPF
+                            <input type="text" name="firstName" value={props.cpf} className='bg-transparent border-none text-3xl text-[#000]' />
+                        </label>
+                        <label className='flex flex-col text-xl text-[#A9A9A9]'>
+                            Celular
+                            <input type="text" name="firstName" value={props.celular} className='bg-transparent border-none text-3xl text-[#000]' />
+                        </label>
 
                     </div>
                 
                     <div className='flex flex-col gap-5 mt-10'>
-                        <p className='text-xl text-[#A9A9A9]'> Último nome
-                            <div className='text-3xl text-[#000]'>{props.lastName}</div>
-                        </p>
-
-                        <p className='flexflex-col text-xl text-[#A9A9A9] '> RG
-                            <div className='text-3xl text-[#000]'> {props.rg}</div>
-                        </p>
-                        
-                        <p className='text-xl text-[#A9A9A9]'> Telefone
-                            <div className='text-3xl text-[#000]'> {props.telefone}</div>
-                        </p>
+                    <label className='flex flex-col text-xl text-[#A9A9A9]'>
+                            Último nome
+                            <input type="text" name="firstName" value={props.lastName} className='bg-transparent border-none text-3xl text-[#000]' />
+                        </label>
+                        <label className='flex flex-col text-xl text-[#A9A9A9]'>
+                            RG
+                            <input type="text" name="firstName" value={props.rg} className='bg-transparent border-none text-3xl text-[#000]' />
+                        </label>
+                        <label className='flex flex-col text-xl text-[#A9A9A9]'>
+                            Telefone
+                            <input type="text" name="firstName" value={props.telefone} className='bg-transparent border-none text-3xl text-[#000]' />
+                        </label>
                     </div>
                     <div className='content-start pr-20 mt-10'>
                         <button className='w-52 h-12 flex flex-row justify-center items-center gap-4 bg-[#ECECEC] rounded-full drop-shadow-lg'>
@@ -45,14 +49,9 @@ export const Pessoais = (props) => {
 
                 </div>
                     <p className='flex flex-col text-xl text-[#A9A9A9] pt-20'> Bio
-                        <span className='text-2xl text-[#000] w-2/4 break-words '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                            blandit tincidunt urna sit amet ultricies. Nulla nec turpis ultrices,
-                            consequat nunc ut, ultrices purus. Maecenas fermentum, metus sit amet
-                            molestie faucibus, justo massa lobortis orci, eget posuere nibh lectus
-                            nec elit. Nam blandit placerat semper. Nam vel ultricies mauris. Pellentesque
-                            eu imperdiet turpis. Nam congue erat orci, vitae varius eros dictum nec. Suspendisse
-                            pharetra nisl sit amet augue suscipit tincidunt. In egestas ex vitae ipsum aliquet
-                            porttitor. Cras efficitur dolor est, quis auctor sapien accumsan sed. Morbi tristique vitae quam eu pretium.
+                        <span className='text-2xl text-[#000] w-2/4 break-words '>
+                        <input type="text" name="firstName" value={props.text} className='bg-transparent border-none w-full h-full' />
+    
                         </span>
                     </p>
                 </div>
