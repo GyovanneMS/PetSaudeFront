d5782abee7a91117b32668508c97b553f26cae6f
Yasmin Santos <yasminnas989@gmail.com>
2023-04-03 11:23:57 -0300

    responsive upgradeConfig

--- src/pages/profile/resource/editUser/responseUser/security.jsx
+++ src/pages/profile/resource/editUser/responseUser/security.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Config} from "../../editUser/headerConfig";
 
 export const SegurancaInfo = () => {
     return (
         <>
-
+            <Config/>
         </>
     );
 }
