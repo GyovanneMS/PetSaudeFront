4d997ed439d3a4d19b7aaf8d53f54d25f456e0ea
LUCAS CAMILO RAMALHO <98404472+LusCamilo@users.noreply.github.com>
2023-04-05 11:20:17 -0300

    update infosUser and profile Photo

--- src/pages/profile/resource/editProfile/infosProfile.jsx
+++ src/pages/profile/resource/editProfile/infosProfile.jsx
@@ -1,39 +1,100 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import lapis from "../../../../assets/svg/pencil.svg"
+import { UserPhoto } from './userPhoto';
 
 
 export const InfosProfile = (props) => {
 
+    const [userInfosDisable, userInfosDisableState] = useState({
+        disable: true, 
+        class:'opacity-50 bg-gray-200'
+    })
 
-    const [userInfos, userinfosState] = useState("disabled:opacity-50")
-    const [password, passwordState] = useState("disabled:opacity-50")
+    const [password, passwordState] = useState({
+        class: "opacity-50 bg-gray-200",
+        disabled: true,
+        type: "password",
+    })
+
+    const [form, formState] = useState({
+        nome: props.nome,
+        email: props.email,
+    })
+
+    const [senha, setSenha] = useState(props.password)
+
+    const handleNameChange = (event) => {
+        console.log(event.target.value);
+        formState({nome: event.target.value});
+    }
+    const handleEmailChange = (event) => {
+        console.log(event.target.value);
+        formState({email: event.target.value});
+    }
+    const handlePasswordChange = (event) => {
+        console.log(event.target.value);
+        setSenha(event.target.value);
+    }
+
 
     return (
-        <div className='w-full h-full border-2 rounded-lg border-black flex flex-col gap-10 pl-20 py-8'>
+        <div className='w-full h-full border-2 rounded-lg border-black flex flex-col gap-10 p-8'>
             <h2 className='text-6xl font-sans'>Informações do perfil</h2>
             <div className="flex justify-between items-center">
                 <div className='flex flex-col text-xl text-[#A9A9A9]'>
                     Nome de usuario
-                    <input className={`text-black text-3xl ${userInfos}`}/>
+                    <input disabled={userInfosDisable.disable} onChange={handleNameChange} className={`text-black text-3xl ${userInfosDisable.class}`} value={form.nome} id="userInfo" />
                 </div>
                 <div className='flex flex-col text-xl text-[#A9A9A9]'>
                     E-mail
-                    <input className={`text-black text-3xl ${userInfos}`}/>
+                    <input disabled={userInfosDisable.disable} onChange={handleEmailChange} className={`text-black text-3xl ${userInfosDisable.class}`} value={form.email} />
                 </div>
-                <button className='w-52 h-12 flex flex-row justify-center items-center gap-4 bg-[#ECECEC] rounded-full drop-shadow-lg'>
-                    <img src={lapis} alt="" />
-                            Editar
+                <button className='w-52 h-12 flex flex-row justify-center items-center gap-4 bg-[#ECECEC] rounded-full drop-shadow-lg' onClick={() => {
+
+                    if (document.getElementById('userInfo').disabled == true ) {
+                        userInfosDisableState({
+                            disable:false, 
+                            class:''
+                        })
+                    }else{
+                        userInfosDisableState({
+                            disable:true,  
+                            class:'opacity-50 bg-gray-200'
+                        })
+                    }
+
+                }}>
+                    <img src={lapis}/>
+                    Editar
                 </button>
             </div>
             <div className="flex justify-between">
                 <div className='flex flex-col text-xl text-[#A9A9A9]'>
                     Senha
-                    <input className={`text-black text-3xl ${password}`}/>
+                    <input type={password.type} id='password' onChange={handlePasswordChange} className={`text-black text-3xl ${password.class}`} value={`${senha}`} />
                 </div>
-                <button className='w-52 h-12 flex flex-row justify-center items-center gap-4 bg-[#F9DEDC] rounded-full drop-shadow-lg font-bold text-[#410E0B]'>
+                <button 
+                    className='w-52 h-12 flex flex-row justify-center items-center gap-4 bg-[#F9DEDC] rounded-full drop-shadow-lg font-bold text-[#410E0B]'
+                    onClick={() => {
+                        if (document.getElementById('password').type == 'password' ) {
+                            passwordState({
+                                type: 'text',
+                                disabled: false,
+                                class: ''
+                            })
+                            console.log();
+                        }else{
+                            passwordState({
+                                type:'password',
+                                disabled: true,
+                                class: 'opacity-50 bg-gray-200'
+                            })
+                        }
+                    }}>
                     Mudar Senha
                 </button>
             </div>
+            <UserPhoto photoUser={props.photoUser} nome={props.nome}/>
         </div>
     );
 }
--- src/pages/profile/resource/editProfile/topContainer.jsx
+++ src/pages/profile/resource/editProfile/topContainer.jsx
@@ -1,33 +1,25 @@
-import React, {useState} from 'react';
-import profilePhoto from "../img/profilePhoto.png";
+import React, { useState } from 'react';
 import more from "../img/more.png"
 
 
-export const TopContainer = () => {
+export const TopContainer = (props) => {
 
-    
-    const [selectedImage, setSelectedImage] = useState();
+    const [selectedFile, setSelectedFile] = useState(props.profilePhoto);
 
-    const imageChange = (e) => {
-        if (e.target.files && e.target.files.length > 0) {
-            setSelectedImage(e.target.files[0]);
-        }
-    };
+    const handleFileInputChange = (event) => {
+        console.log(event.target.files[0])
+        const file = event.target.files[0]
+        setSelectedFile(URL.createObjectURL(file));
+    }
 
     return (
         <div className="w-full">
-            <input type="file" name="photo" id="photoProfile" className="hidden" />
-            <label htmlFor='photoProfile' style={{ backgroundImage: `url(${profilePhoto})` }}
+            <input type="file" accept="image/*" name="photo" id="photoProfile" className="hidden" onChange={handleFileInputChange} />
+            <label htmlFor='photoProfile' style={{backgroundImage: `url(${selectedFile})`}}
                 className='
-                    flex justify-center items-center ease-in duration-300 w-full h-64 bg-center bg-origin-content bg-no-repeat bg-cover cursor-pointer rounded-lg bg-zinc-500 hover:bg-blend-lighten '>
-                {selectedImage && (
-                    <img
-                        src={URL.createObjectURL(selectedImage)}
-                    />
-                )}
+                    flex justify-center items-center bg-slate-200 w-full h-64 bg-center bg-origin-content bg-no-repeat bg-cover cursor-pointer rounded-lg hover:bg-blend-darken '>
                 <img className src={more} />
             </label>
-
         </div>
     );
 }
